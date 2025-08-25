4ce00d552348e2eff835e89d8fd2495cb8590e5d
Daniela Homobono <110701158+danielaHomobono@users.noreply.github.com>
2025-08-25 10:58:07 -0300

    implement wipe funtionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         loadResumes()
     }, []);
 
-    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    return <main className="bg-[url('/images/fondo-colorido-borroso-vivo.jpg')] bg-cover">
         <Navbar />
 
         <section className="main-section">
